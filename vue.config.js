725eb9220caf0856f9af941994ba762169ac6b32
Bahodir Madatov <n.b.madatov@gmail.com>
2024-06-04 13:12:55 +0500

    adding dist

--- vue.config.js
+++
@@ -1,5 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  publicPath: '/Rick-Morty/', // Replace with your repository name
-  transpileDependencies: true
-})
